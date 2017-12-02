b10dbdfd830dd563551bcc22792ddcb560e1d1b2
srf94 <samfawcett@googlemail.com>
2017-12-02 10:19:17 +0000

    Day 2 (python)

---
+++ python/2.py
@@ -0,0 +1,25 @@
+import itertools
+
+with open('input/day2.txt', 'r') as f:
+	data = f.read()
+
+# Remove \t and \n from input and remove empty rows
+rows = [[int(j) for j in i.split("\t") if j] 
+         for i in data.split("\n")]
+rows = [i for i in rows if i]
+
+checksum = sum(max(row) - min(row) for row in rows)
+print "Part 1: %s" % checksum
+
+checksum = 0
+for row in rows:
+	for i, j in itertools.combinations(row, 2):
+		a = min(i, j)
+		b = max(i, j)
+		if not b % a:
+			# Have found our matching pair
+			checksum += b / a
+			break
+
+print "Part 2: %s" % checksum
+
