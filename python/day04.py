894b6e9d533cf0f0d9022effcdd9e45996e92d6f
srf94 <samfawcett@googlemail.com>
2017-12-05 11:04:23 +0000

    python day 4

---
+++ python/day04.py
@@ -0,0 +1,23 @@
+
+with open('input/day04.txt', 'r') as f:
+	data = f.read()
+
+data = data.splitlines()
+
+def answer(data, anagram):
+	valid_phrases = 0
+	for line in data:
+		if anagram:
+			words = ["".join(sorted(i)) for i in line.split(" ")]
+		else:
+			words = line.split(" ")
+
+		count = set(words)
+		if len(words) == len(count):
+			valid_phrases += 1
+
+	return valid_phrases
+
+print "Part 1: %s" % answer(data, False)
+print "Part 2: %s" % answer(data, True)
+
