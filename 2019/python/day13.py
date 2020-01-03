43f86c403db364fef6f28ec0745159e2d7a6e2dd
srf94 <samfawcett@googlemail.com>
2020-01-03 01:30:41 +0000

    Use intcode class for day 13

--- 2019/python/day13.py
+++ 2019/python/day13.py
@@ -1,17 +1,19 @@
 from copy import copy
 from utils import read_data
-from intcode_vm import run_instructions, intcode_send
+from intcode_vm import IntcodeVM
 
 
 def draw_board(tiles):
     tiles = copy.copy(tiles)
     for tile in tiles:
         for loc in range(len(tile)):
-            tile[loc] = str(tile[loc]). \
-                replace('0', ' '). \
-                replace('2', 'B'). \
-                replace('3', '_'). \
-                replace('4', 'O')
+            tile[loc] = (
+                str(tile[loc])
+                .replace("0", " ")
+                .replace("2", "B")
+                .replace("3", "_")
+                .replace("4", "O")
+            )
 
     print("\n".join("".join(str(i) for i in tile) for tile in tiles))
 
@@ -26,12 +28,8 @@ def print_tuple(x, y, tile):
     print("{}: x: {}, y: {}".format(name, x, y))
 
 
-def single_step(value, gen):
-    triple = []
-    for i in range(3):
-        value = intcode_send(gen, value)
-        triple.append(value)
-    return tuple(triple)
+def single_step(value, vm):
+    return vm.run(value), vm.run(), vm.run()
 
 
 def get_location(board, last, paddle=False, ball=False):
@@ -52,17 +50,17 @@ def ball_intersection(paddle_loc, ball_loc, ball_direction):
     return ball_loc[0] + (paddle_loc[1] - ball_loc[1] - 1) * ball_direction
 
 
-def create_board(generator_input, gen, dim_x, dim_y):
+def create_board(vm_input, vm, dim_x, dim_y):
     total_pixels = dim_x * dim_y
     board = [[0] * dim_x for _ in range(dim_y)]
 
     for _ in xrange(total_pixels):
-        x, y, tile = single_step(generator_input, gen)
+        x, y, tile = single_step(vm_input, vm)
         board[y][x] = tile
     return board
 
 
-def play_game(board):
+def play_game(board, vm):
     ball_direction = 1
     ball_loc = None
     paddle_loc = None
@@ -88,9 +86,8 @@ def play_game(board):
         else:
             direction = 0
 
-        try:
-            x, y, tile = single_step(direction, gen)
-        except:
+        x, y, tile = single_step(direction, vm)
+        if x is None:
             return last_score
 
         if x == -1:
@@ -104,16 +101,15 @@ dim_x = 44
 dim_y = 20
 
 
-gen = run_instructions(raw, debug=False)
-board = create_board(0, gen, dim_x, dim_y)
+vm = IntcodeVM(raw)
+board = create_board(0, vm, dim_x, dim_y)
 print("Part 1:")
 print(sum(sum(i == 2 for i in row) for row in board))
 
 
-raw[0] = 2
-gen = run_instructions(raw, debug=False)
-board = create_board(0, gen, dim_x, dim_y)
+vm = IntcodeVM(raw, mutate_input={0: 2})
+board = create_board(0, vm, dim_x, dim_y)
 
 
 print("Part 2:")
-print(play_game(board))
+print(play_game(board, vm))
