3646b42df6cfc1ae81bbf3ed7dcaf9cfca39f90a
srf94 <samfawcett@googlemail.com>
2020-01-05 23:39:17 +0000

    day 17 python

--- 2019/python/day17.py
+++ 2019/python/day17.py
@@ -1,5 +1,9 @@
+from itertools import product
 from utils import read_data
 from intcode.vm import IntcodeVM
+
+
+raw = read_data(17)[0].split(",")
 
 
 def draw_grid(square):
@@ -25,7 +29,8 @@ def get_reverse(d):
     return {1: 2, 2: 1, 3: 4, 4: 3}[d]
 
 
-def get_grid(raw, vm):
+def get_grid(raw):
+    vm = IntcodeVM(raw, mutate_input={0: 1})
     flat_grid = vm.collect_all_outputs()
 
     g, row = [], []
@@ -40,8 +45,10 @@ def get_grid(raw, vm):
 
 def part_1(grid):
     scaffolding = {
-        (x, y) for y, row in enumerate(grid) for x, value in enumerate(row)
-        if value == '#'
+        (x, y)
+        for y, row in enumerate(grid)
+        for x, value in enumerate(row)
+        if value == "#"
     }
 
     total = 0
@@ -52,15 +59,150 @@ def part_1(grid):
         else:
             total += position[0] * position[1]
 
-    print("Part 1:")
-    print(total)
+    return total
 
 
-raw = read_data(17)[0].split(",")
+def find_start(grid):
+    for (y, row) in enumerate(grid):
+        for (x, value) in enumerate(row):
+            if value == "^":
+                return x, y
+    assert False
 
 
-raw[0] = 1
-vm = IntcodeVM(raw, mutate_input={0: 1})
-grid = get_grid(raw, vm)
+def find_path_needed(grid):
+    path = []
+    direction = 2
+    loc = find_start(grid)
 
-part_1(grid)
+    while True:
+        possible = []
+        for d in range(4):
+            if d % 2 == direction % 2:
+                continue
+            new = add_direction(loc, d)
+            try:
+                if grid[new[1]][new[0]] == "#":
+                    possible.append(d)
+            except IndexError:
+                pass
+
+        if len(possible) != 1:
+            break
+
+        new_direction = possible[0]
+        assert (new_direction - direction) % 4 in [1, 3]
+        turn = "L" if (new_direction - direction) % 4 == 1 else "R"
+
+        count = 0
+        while True:
+            new = add_direction(loc, new_direction)
+            try:
+                if grid[new[1]][new[0]] != "#":
+                    break
+            except IndexError:
+                break
+            loc = new
+            count += 1
+
+        direction = new_direction
+        path.append(turn + str(count))
+
+    return path
+
+
+def get_joined_pattern(pattern):
+    return ",".join("{},{}".format(i[0], i[1:]) for i in pattern) + "\n"
+
+
+def apply_next_pattern(letter, length, coverage, path):
+    N = len(path)
+
+    # Get pattern from first non-None value
+    start = next(i for i, v in enumerate(coverage) if v is None)
+    pattern = path[start : start + length]
+
+    joined_pattern = get_joined_pattern(pattern)
+    if 20 < len(joined_pattern):
+        return False, False
+
+    all_none = all(i is None for i in coverage[start : start + length])
+    if not all_none:
+        return False, False
+
+    for i in range(length):
+        coverage[start + i] = letter
+
+    loc = 0
+    while True:
+        if N < loc + length:
+            break
+
+        match = path[loc : loc + length] == pattern
+        all_none = all(i is None for i in coverage[loc : loc + length])
+
+        if match and all_none:
+            for i in range(length):
+                coverage[loc + i] = letter
+            loc += length
+        else:
+            loc += 1
+
+    return joined_pattern, coverage
+
+
+def find_covering_routine(path):
+    # Shortest possible step is 'L,4,' so at most 5 can fit into a routine of length 20
+    max_len = 5
+    letters = ["A", "B", "C"]
+    chosen_patterns = None
+
+    # Iterate over all possible patterns until we find a perfect covering - i.e. patterns
+    # that are sufficiently short and cover every t
+    possible_length_combinations = (range(2, max_len) for _ in range(len(letters)))
+    for lengths in product(*possible_length_combinations):
+        coverage = [None] * len(path)
+        chosen_patterns = []
+
+        for i, letter in enumerate(letters):
+            pattern, coverage = apply_next_pattern(letter, lengths[i], coverage, path)
+            if not pattern:
+                break
+            chosen_patterns.append(pattern)
+        else:
+            if None not in coverage:
+                break
+
+    if chosen_patterns is None:
+        raise Exception("Could not find solution for part 2")
+
+    loc = 0
+    routine = []
+    while loc < len(coverage):
+        letter = coverage[loc]
+        routine.append(letter)
+        loc += lengths[letters.index(letter)]
+    routine = ",".join(routine) + "\n"
+
+    return routine, chosen_patterns
+
+
+def part_2(grid):
+    path = find_path_needed(grid)
+    routine, chosen_patterns = find_covering_routine(path)
+    video = "y\n"
+    text_input = [ord(i) for i in routine + "".join(chosen_patterns) + video]
+    vm = IntcodeVM(raw, mutate_input={0: 2}, input_=text_input)
+    output = vm.collect_all_outputs()
+    return output[-1]
+
+
+full_grid = get_grid(raw)
+
+
+print("Part 1:")
+print(part_1(full_grid))
+
+
+print("Part 2:")
+print(part_2(full_grid))
